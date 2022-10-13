2d53530b93f7bc37b59fd4a1ee4f25f58e1964ec
Lamine ADEL <lamine.adel@ynov.com>
2022-10-14 00:16:35 +0200

    herokuTest

--- src/services/service.service.ts
+++ src/services/service.service.ts
@@ -1,10 +1,12 @@
-import express from "express";
 import { port } from "../configs/index.config";
 import { notesRouter } from "../routes/notes.route";
 import { usersRouter } from "../routes/users.route";
 import { foldersRouter } from "../routes/folder.route";
 import cors from "cors";
-import bodyParser from "body-parser";
+
+const bodyParser = require("body-parser");
+
+const express = require("express");
 
 const app = express();
 
