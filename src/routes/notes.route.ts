2093a904c242459c119713399c0301e46acd5553
Lamine ADEL <lamine.adel@ynov.com>
2022-10-14 00:28:43 +0200

    herokutest

--- src/routes/notes.route.ts
+++ src/routes/notes.route.ts
@@ -1,6 +1,6 @@
-import express from "express";
 import { createNote } from "../controllers/note.controller";
 
+const express = require("express");
 export const notesRouter = express.Router();
 
 notesRouter.post("/notes/create-note", createNote);
