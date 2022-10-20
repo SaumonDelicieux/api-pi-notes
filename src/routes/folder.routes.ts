af1c56d1fa6ffac5b6d092dff54a1ff6b88c4dc2
Walid <mokhtariwalid@hotmail.com>
2022-10-20 16:58:39 +0200

    Adding update routes

--- src/routes/folder.routes.ts
+++ src/routes/folder.routes.ts
@@ -2,6 +2,7 @@ import express from "express";
 import {
   createFolder,
   deleteFolder,
+  updateFolderTitle,
   getFolders,
 } from "../controllers/folder.controller";
 
@@ -9,4 +10,5 @@ export const folderRouter = express.Router();
 
 folderRouter.post("/folders/createFolder", createFolder);
 folderRouter.get("/folders/getAll", getFolders);
+folderRouter.put("/folders/updateFolderTitle", updateFolderTitle);
 folderRouter.delete("/folders/deleteFolder", deleteFolder);
