18ee310bac2e7f9b3a17cd7becef4b148bd4f701
Lamine ADEL <lamine.adel@ynov.com>
2022-10-27 23:50:42 +0200

    addCron

--- src/models/token.model.ts
+++ src/models/token.model.ts
@@ -6,17 +6,17 @@ const Schema = mongoose.Schema;
 const tokenSchema = new Schema({
   userId: {
     type: Schema.Types.ObjectId,
-    ref: 'User',
-    require: true
+    ref: "User",
+    require: true,
   },
   token: {
     type: String,
-    require: true
+    require: true,
   },
   createdAt: {
     type: Date,
     default: Date.now(),
-    expires: 3600
+    expires: 86400,
   },
 });
 
