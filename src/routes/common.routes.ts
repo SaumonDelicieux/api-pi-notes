bd91f441e4cf731cb68c1142472ac92876ed5545
Walid <mokhtariwalid@hotmail.com>
2022-11-03 12:53:26 +0100

    Add delete from shared with and refacto

--- src/routes/common.routes.ts
+++ src/routes/common.routes.ts
@@ -1,8 +1,9 @@
 import express from "express";
-import { getEmailToShare, shareNote } from "../controllers/common.controller";
+import { getEmailToShare, shareNote, deleteFromSharedWith } from "../controllers/common.controller";
 import { verifyToken } from "../helpers/verifyToken";
 
 export const commonRouter = express.Router();
 
 commonRouter.get("/commons/getEmailsToShare", verifyToken, getEmailToShare);
 commonRouter.post("/commons/shareNote", verifyToken, shareNote);
+commonRouter.post("/commons/deleteFromSharedWith", verifyToken, deleteFromSharedWith);
